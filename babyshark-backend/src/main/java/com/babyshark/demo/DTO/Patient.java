f0284489d162793e2b7cabbe2fe1161db13a48f7
mavric <mavr0001@e.ntu.edu.sg>
2018-09-26 22:09:02 +0800

    Refactor some names Added lombok.

--- babyshark-backend/src/main/java/com/babyshark/demo/DTO/Patient.java
+++ babyshark-backend/src/main/java/com/babyshark/demo/DTO/Patient.java
@@ -1,7 +1,17 @@
 package com.babyshark.demo.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Patient {
+    @Getter
+    @Setter
     private String NRIC;
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
+    private int age = 10;
     
 }
