8548a27790964b55ec5acc01fb480fa520878697
Zhi Jie <zhijie12@gmail.com>
2018-09-28 23:42:55 +0800

    Added Wifi Manager to get list of Wifi

--- babyshark-android/app/src/main/java/com/babyshark/amazon_kkh_navigator/MainActivity.java
+++ babyshark-android/app/src/main/java/com/babyshark/amazon_kkh_navigator/MainActivity.java
@@ -1,13 +1,32 @@
 package com.babyshark.amazon_kkh_navigator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    WifiManager wifiManager;
+    List<ScanResult> results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    public void init() {
+        // Init Wifi Class
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        results = wifiManager.getScanResults();
+        Log.d("All Result", "Result:" + results.toString());
+    }
+
 }
+
